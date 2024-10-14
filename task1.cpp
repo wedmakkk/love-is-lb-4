03904161d3f2006808507d6dd182ded619706c10
wedmakkk <63804210+wedmakkk@users.noreply.github.com>
2024-10-14 17:58:26 +0300

    task 1

---
+++ task1.cpp
@@ -0,0 +1,114 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <locale.h>
+
+struct Node {
+	int data;
+	struct Node* left;
+	struct Node* right;
+};
+struct Node* root;
+
+struct Node* CreateTree(struct Node* root, struct Node* r, int data)
+{
+	if (r == NULL)
+	{
+		r = (struct Node*)malloc(sizeof(struct Node));
+		if (r == NULL)
+		{
+			printf("Ошибка выделения памяти");
+			exit(0);
+		}
+
+		r->left = NULL;
+		r->right = NULL;
+		r->data = data;
+		if (root == NULL) return r;
+
+		if (data > root->data)	root->left = r;
+		else root->right = r;
+		return r;
+	}
+
+	if (data > r->data)
+		CreateTree(r, r->left, data);
+	else
+		CreateTree(r, r->right, data);
+
+	return root;
+}
+
+void print_tree(struct Node* r, int l)
+{
+
+	if (r == NULL)
+	{
+		return;
+	}
+
+	print_tree(r->right, l + 1);
+	for (int i = 0; i < l; i++)
+	{
+		printf(" ");
+	}
+
+	printf("%d\n", r->data);
+	print_tree(r->left, l + 1);
+}
+void find(int num)
+{
+	while (root)
+	{
+		if (root->data == num)
+		{
+			printf("Число найдено!!!");
+			return;
+		}
+		if (root->data > num)
+		{
+			root = root->right;
+		}
+		if (root->data < num)
+		{
+			root = root->left;
+		}
+	}
+	printf("число не найдено!!!");
+	return ;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int D, start = 1;
+
+	root = NULL;
+	printf("-1 - окончание построения дерева\n");
+	while (start)
+	{
+		printf("Введите число: ");
+		scanf_s("%d", &D);
+		if (D == -1)
+		{
+			printf("Построение дерева окончено\n\n");
+			start = 0;
+		}
+		else
+			root = CreateTree(root, root, D);
+
+	}
+
+	print_tree(root, 0);
+
+	scanf_s("%d", &D);
+
+	printf("\nВведите число: ");
+	int num;
+	scanf_s("%i", &num);
+	find(num);
+	return 0;
+}
+
